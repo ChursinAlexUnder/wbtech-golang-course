f00d3903a18104c1eaeedc770411e9fbe2c3a582
ChursinAlexUnder <keks63620@gmail.com>
2025-08-28 22:41:47 +0300

    Completed L1.2

--- L1/task2/main.go
+++ L1/task2/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func inSquare(number int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(number * number)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	var (
+		mas []int           = []int{2, 4, 6, 8, 10}
+		wg  *sync.WaitGroup = new(sync.WaitGroup)
+	)
+	for _, value := range mas {
+		wg.Add(1)
+		go inSquare(value, wg)
+	}
+	wg.Wait()
 }
